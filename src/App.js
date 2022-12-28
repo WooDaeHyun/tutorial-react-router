45204ddba6ce3e068777654b18f73b7fab6d30c8
WooDaeHyun <po668312@naver.com>
2022-12-28 16:01:53 +0900

    feat: react router 설치 및 연습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import { Route, Routes } from "react-router-dom";
+import Profile from "./pages/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* 모든 페이지에 보이고 싶으면 Routs 컴포넌트 바깥 영역에서 장석하면 된다 */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* 주소가 /일때는 Home 컴포넌트를 */}
+        <Route path="/about" element={<About />} />
+        {/* 주소가 /about 일때는 About 컴포넌트를 */}
+      </Routes>
     </div>
   );
 }
