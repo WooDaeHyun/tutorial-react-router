bb200e41edc48b1c1d6ac49646c54ef1959b6b50
WooDaeHyun <po668312@naver.com>
2022-12-28 21:28:40 +0900

    feat: useNavigate 적용

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { Route, Routes } from "react-router-dom";
 import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
+import Layout from "./pages/Layout";
 
 function App() {
   return (
     <div>
       {/* 모든 페이지에 보이고 싶으면 Routs 컴포넌트 바깥 영역에서 장석하면 된다 */}
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* 주소가 /일때는 Home 컴포넌트를 */}
-        <Route path="/about" element={<About />} />
-        {/* 주소가 /about 일때는 About 컴포넌트를 */}
-        <Route path="/profiles/:username" element={<Profile />} />
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          {/* 주소가 /일때는 Home 컴포넌트를 */}
+          <Route path="/about" element={<About />} />
+          {/* 주소가 /about 일때는 About 컴포넌트를 */}
+          <Route path="/profiles/:username" element={<Profile />} />
+        </Route>
         <Route path="/articles" element={<Articles />}>
           <Route path=":id" element={<Article />} />
         </Route>
